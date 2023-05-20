2e2ae4414987e1c90bf165b68b9ffed98c79a20c
Victor Queiroz <victorcqueirozg@gmail.com>
2023-05-20 01:11:34 -0300

    several improvements and code coverage increase
    Signed-off-by: Victor Queiroz <victorcqueirozg@gmail.com>

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,3 +1,5 @@
+import { ILogger } from '@jscriptlogger/lib';
+
 type EventCallback =
   | ((value: unknown) => void)
   | ((value: unknown) => Promise<void>);
@@ -12,23 +14,41 @@ export interface IEventEmitterOptions {
   maxListenerWaitTime: number;
   waitListenerPromise: boolean;
   queueIfNoEventListeners: boolean;
+  logger: ILogger;
 }
 
-export default class EventEmitter<EventMap extends Record<string, unknown>> {
+enum EventEmitterState {
+  Started,
+  Stopped,
+}
+
+export default class EventEmitter<EventMap extends Record<string, any>> {
   readonly #queueIfNoEventListeners;
   readonly #maxListenerWaitTime;
   readonly #waitListenerPromise;
   readonly #events = new Map<keyof EventMap, IEvent>();
+  readonly #logger;
+  #state: EventEmitterState;
   public constructor({
     waitListenerPromise = true,
     queueIfNoEventListeners = true,
-    maxListenerWaitTime = 5000,
+    logger,
+    maxListenerWaitTime = 1000,
   }: Partial<IEventEmitterOptions> = {}) {
+    this.#state = EventEmitterState.Started;
     this.#waitListenerPromise = waitListenerPromise;
     this.#maxListenerWaitTime = maxListenerWaitTime;
     this.#queueIfNoEventListeners = queueIfNoEventListeners;
+    this.#logger = logger?.at('EventEmitter');
   }
   public async wait(eventNames: (keyof EventMap)[] = []) {
+    if (this.#state !== EventEmitterState.Started) {
+      this.#logger?.error(
+        'wait called used in a weird manner. event emitter is stopped'
+      );
+      return;
+    }
+    this.#flushEvents(eventNames);
     for (const e of this.#events) {
       if (eventNames.length > 0 && !eventNames.includes(e[0])) {
         continue;
@@ -40,12 +60,11 @@ export default class EventEmitter<EventMap extends Record<string, unknown>> {
     name: K,
     listener: (value: EventMap[K]) => void
   ) {
-    const { listeners, pendingEventEmits } = this.#event(name);
+    const { listeners } = this.#event(name);
     listeners.add(listener as EventCallback);
 
-    for (const value of pendingEventEmits.splice(0, pendingEventEmits.length)) {
-      this.emit(name, value as EventMap[K]);
-    }
+    this.#flushEvents([name]);
+    return this;
   }
   public off<K extends keyof EventMap>(
     name: K,
@@ -55,6 +74,10 @@ export default class EventEmitter<EventMap extends Record<string, unknown>> {
   }
   public emit<K extends keyof EventMap>(name: K, value: EventMap[K]) {
     const e = this.#event(name);
+    if (this.#state === EventEmitterState.Stopped) {
+      e.pendingEventEmits.push(value);
+      return;
+    }
     if (this.#queueIfNoEventListeners) {
       if (!e.listeners.size) {
         e.pendingEventEmits.push(value);
@@ -68,28 +91,71 @@ export default class EventEmitter<EventMap extends Record<string, unknown>> {
        * failures
        */
       if (!this.#waitListenerPromise) {
-        this.#callEventListener(l, value);
+        /**
+         * pause event emitting
+         */
+        this.pause();
+        const result = this.#callEventListener(l, value);
+        e.pending = Promise.all([e.pending, result]).then(() => {});
+        this.resume();
+        this.#flushEvents([name]);
         continue;
       }
       e.pending = Promise.resolve(e.pending).then(
         () =>
           new Promise<void>((resolve) => {
+            const resumeAndResolve = () => {
+              this.resume();
+              this.#flushEvents([name]);
+              resolve();
+            };
             const timeoutId = setTimeout(() => {
-              console.error(
+              this.#logger?.error(
                 'event listener took more than %d ms to be resolved: %o',
                 this.#maxListenerWaitTime,
                 l
               );
-              resolve();
+              resumeAndResolve();
             }, this.#maxListenerWaitTime);
-            this.#callEventListener(l, value)
-              .then(() => {})
-              .finally(() => {
-                clearTimeout(timeoutId);
-                resolve();
-              });
+            this.pause();
+            this.#callEventListener(l, value).finally(() => {
+              clearTimeout(timeoutId);
+              resumeAndResolve();
+            });
           })
       );
+    }
+  }
+  public pause() {
+    if (this.#state !== EventEmitterState.Started) {
+      this.#logger?.error(
+        'pause called when event emitter was already stopped'
+      );
+      return;
+    }
+    this.#state = EventEmitterState.Stopped;
+  }
+  public resume() {
+    if (this.#state !== EventEmitterState.Stopped) {
+      this.#logger?.error(
+        'resume was called, but event emitter was in stopped state'
+      );
+      return;
+    }
+    this.#state = EventEmitterState.Started;
+  }
+  #flushEvents(names: (keyof EventMap)[]) {
+    for (const [name, e] of this.#events) {
+      if (names.length > 0 && !names.includes(name)) {
+        continue;
+      }
+      const pendingDispatches = e.pendingEventEmits.splice(
+        0,
+        e.pendingEventEmits.length
+      );
+      for (const value of pendingDispatches) {
+        this.emit(name, value as EventMap[typeof name]);
+      }
     }
   }
   async #callEventListener<T>(listener: (value: T) => void, value: T) {
@@ -97,19 +163,21 @@ export default class EventEmitter<EventMap extends Record<string, unknown>> {
     try {
       result = listener(value);
     } catch (reason) {
-      console.error('event listener returned threw an exception: %o', reason);
+      this.#logger?.error('event listener call threw an exception: %o', reason);
       result = null;
     }
-    return Promise.resolve(result).catch((reason) => {
-      console.error(
-        'event listener returned a promise that was rejected with error: %o',
-        reason
-      );
-    });
+    return Promise.resolve(result)
+      .then(() => {})
+      .catch((reason) => {
+        this.#logger?.error(
+          'event listener returned a promise that was rejected with error: %o',
+          reason
+        );
+      });
   }
-  #event(name: keyof EventMap) {
+  #event(name: keyof EventMap): IEvent {
     let e = this.#events.get(name);
-    if (!e) {
+    if (typeof e === 'undefined') {
       e = {
         pendingEventEmits: new Array(),
         listeners: new Set(),
--- test/EventEmitter.ts
+++ test/EventEmitter.ts
@@ -3,8 +3,42 @@ import timers from 'timers';
 import { EventEmitter } from '../src';
 import { Suite } from 'sarg';
 import { spy } from 'sinon';
+import Logger from '@jscriptlogger/lib';
+import sinon from 'sinon';
 
 const suite = new Suite();
+
+async function wait(ms: number) {
+  return new Promise<void>((resolve) => {
+    timers.setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+suite.test('it should work if you pass an interface to it', () => {
+  interface IEventMap {
+    a: string;
+    b: number;
+  }
+  new EventEmitter<IEventMap>();
+});
+
+suite.test(
+  'it should support emitting the same event inside event emitter listener',
+  async () => {
+    const fn = spy((_: number) => {});
+    const e = new EventEmitter<{ a: number }>();
+    e.on('a', fn).on('a', (value) => {
+      if (value >= 0) {
+        e.emit('a', value - 1);
+      }
+    });
+    e.emit('a', 4);
+    await e.wait();
+    console.log(fn.args);
+  }
+);
 
 suite.test(
   'it should queue event dispatches if no listener was added',
@@ -100,5 +134,219 @@ suite.test('it should attach two event listeners to the same event', () => {
   e.on('a', l1);
   e.on('a', l2);
 });
+
+suite.test(
+  'it should log if the event listener returns a rejected promise',
+  async () => {
+    const customConsole = {
+      log: sinon.spy(),
+      debug: sinon.spy(),
+      error: sinon.spy(),
+    };
+    const logger = new Logger([], {
+      console: customConsole,
+      root: null,
+    });
+    const e = new EventEmitter<{ a: number }>({
+      logger,
+    });
+    e.on('a', () => Promise.reject(null));
+    e.emit('a', 1);
+    await e.wait();
+    assert.strict.ok(
+      customConsole.error.calledOnceWithExactly(
+        'EventEmitter: event listener returned a promise that was rejected with error: %o',
+        null
+      )
+    );
+  }
+);
+
+suite.test(
+  'it should log if listener show a synchronous failure as well',
+  async () => {
+    const customConsole = {
+      log: sinon.spy(),
+      debug: sinon.spy(),
+      error: sinon.spy(),
+    };
+    const logger = new Logger([], {
+      console: customConsole,
+      root: null,
+    });
+    const e = new EventEmitter<{ a: number }>({
+      logger,
+    });
+    e.on('a', () => {
+      throw 1;
+    });
+    e.emit('a', 1);
+    await e.wait();
+    assert.strict.ok(
+      customConsole.error.calledOnceWithExactly(
+        'EventEmitter: event listener call threw an exception: %o',
+        1
+      )
+    );
+  }
+);
+
+suite.test(
+  'EventEmitter#resume: it should log if resume is called when it is already in resume state',
+  async () => {
+    const customConsole = {
+      log: sinon.spy(),
+      debug: sinon.spy(),
+      error: sinon.spy(),
+    };
+    const logger = new Logger([], {
+      console: customConsole,
+      root: null,
+    });
+    const e = new EventEmitter<{ a: number }>({
+      logger,
+    });
+    e.resume();
+    await e.wait();
+    assert.strict.ok(
+      customConsole.error.calledOnceWithExactly(
+        'EventEmitter: resume was called, but event emitter was in stopped state'
+      )
+    );
+  }
+);
+
+suite.test(
+  'EventEmitter#pause: it should log if pause is called when event emitter is already paused',
+  async () => {
+    const customConsole = {
+      log: sinon.spy(),
+      debug: sinon.spy(),
+      error: sinon.spy(),
+    };
+    const logger = new Logger([], {
+      console: customConsole,
+      root: null,
+    });
+    const e = new EventEmitter<{ a: number }>({
+      logger,
+    });
+    e.pause();
+    e.pause();
+    assert.strict.ok(
+      customConsole.error.calledOnceWithExactly(
+        'EventEmitter: pause called when event emitter was already stopped'
+      )
+    );
+  }
+);
+
+suite.test(
+  'it should resolve the listener if it takes longer than max wait time',
+  async () => {
+    const customConsole = {
+      log: sinon.spy(),
+      debug: sinon.spy(),
+      error: sinon.spy(),
+    };
+    const logger = new Logger([], {
+      console: customConsole,
+      root: null,
+    });
+    const e = new EventEmitter<{ a: number }>({
+      logger,
+      maxListenerWaitTime: 10,
+    });
+    const listener = () =>
+      new Promise<void>((resolve) =>
+        timers.setTimeout(() => {
+          resolve();
+        }, 100)
+      );
+    e.on('a', listener);
+    e.emit('a', 1);
+    await e.wait();
+    assert.strict.ok(
+      customConsole.error.calledOnce &&
+        customConsole.error.calledWithMatch(
+          /event listener took more than %d ms to be resolved/
+        )
+    );
+  }
+);
+
+suite.test('it should flush just specific event names', () => {
+  const e = new EventEmitter<{
+    a: number;
+    b: string;
+  }>();
+  e.emit('a', 1);
+  e.emit('b', '...');
+  e.on('a', () => {});
+});
+
+suite.test('it should handle listeners that return promises', async () => {
+  let n = 0;
+  interface Events {
+    setValue: {
+      expectedValue: number;
+      newValue: number;
+    };
+  }
+  const e = new EventEmitter<Events>();
+  e.on('setValue', async ({ expectedValue, newValue }) => {
+    assert.strict.equal(expectedValue, n);
+    n = newValue;
+    await wait(10);
+  });
+  for (let i = 0; i < 10; i++) {
+    e.emit('setValue', {
+      expectedValue: i,
+      newValue: i + 1,
+    });
+  }
+  await e.wait();
+});
+
+suite.test(
+  'it should handle cases where the options do not want us to wait for listeners',
+  () => {
+    const e = new EventEmitter<{
+      a: number;
+    }>({
+      waitListenerPromise: false,
+    });
+    e.on('a', () => wait(10));
+    e.on('a', () => wait(10));
+    e.on('a', () => wait(10));
+    e.on('a', () => wait(10));
+    e.emit('a', 1);
+  }
+);
+
+suite.test(
+  'it should log if wait is called when event emitter is in paused state',
+  async () => {
+    const customConsole = {
+      log: sinon.spy(),
+      debug: sinon.spy(),
+      error: sinon.spy(),
+    };
+    const logger = new Logger([], {
+      console: customConsole,
+      root: null,
+    });
+    const e = new EventEmitter<{ a: number }>({
+      logger,
+    });
+    e.pause();
+    await e.wait();
+    assert.strict.ok(
+      customConsole.error.calledOnceWithExactly(
+        'EventEmitter: wait called used in a weird manner. event emitter is stopped'
+      )
+    );
+  }
+);
 
 export default suite;
